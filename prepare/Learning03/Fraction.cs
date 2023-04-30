3f104fdd8e6acf06c21c033929c5e9b64ac2d5e5
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-04-30 11:54:40 -0400

    Encapsulation Learning Activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,54 @@
+class Fraction 
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top) 
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom) 
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int getTop()
+    {
+        return _top;
+    }
+
+    public int getBottom()
+    {
+        return _bottom;
+    }
+
+    public void setTop(int number)
+    {
+        _top = number;
+    }
+
+    public void setBottom(int number)
+    {
+        _bottom = number;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
